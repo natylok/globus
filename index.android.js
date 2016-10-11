a1298d17030096dd6b031af9f5bbadd56e4f7cbd
Nati Levy <nati@cooladata.com>
2016-10-12 00:17:22 +0300

    First commit

--- index.android.js
+++ index.android.js
@@ -14,24 +14,4 @@ class reactNatiNati extends Component {
     );
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
-
 AppRegistry.registerComponent('reactNatiNati', () => reactNatiNati);
---
+++ src/components/logo.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export default class logo extends React.Component {
+	
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div></div>
+    );
+  }
+}
